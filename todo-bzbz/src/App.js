397ceb0a82fc42a7dd53e39f5b9a54dd898d1ee5
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-03-22 19:07:00 +0400

    add sevagirner of 23

---
+++ todo-bzbz/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+      <div className='container'>
+        <div className='alltypelists'>
+          <h3>Todo Lists</h3>
+          <input type='text' className='search' placeholder='search'>
+          </input>
+          <div className='typelists'>
+                a
+                a
+                a
+          </div>
+          </div>
+        <div className='todolists'>
+        todo blah
+        todo blah blah
+        </div>
+        </div>
+      </div>
+);
+}
+}
+
+export default App;
